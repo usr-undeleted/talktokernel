880bfd07d9f183468143b96198b8d3f21b5f44bf
Undeleted <zephimhere@gmail.com>
2026-02-19 17:30:20 -0300

    Starting code out

---
+++ src/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char* argv[]) {
+	char *words[] = {"For","While","If","Else","Declare","Allocate","Memory"};
+
+	if (argc == 2) {
+
+	} else {
+		printf("Error! Did you type just a number for the second arg (if you typed an arg...)?");
+		return 1;
+	}
+}
