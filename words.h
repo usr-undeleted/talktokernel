80a5aac70a01a0bc39ae3a7e083ce594573ff99b
Undeleted <zephimhere@gmail.com>
2026-02-20 14:11:45 -0300

    1.3.3

--- words.h
+++ words.h
@@ -4,12 +4,12 @@
 static const char *words[] = {"Declare","Allocate","Memory", "True", "False", "No", "Yes", "Terrible", "Bad", "Ok", "Good", "Amazing", "Love", "Hate", "Friend", "Foe", "Organic", "Eletronic", "Machine", "Human", "Embrace", "Reject", "Accept", "Fear", "Thrill", "Kill", "Help", "Linux", "Kernel", "Arch", "Debian", "Gentoo", "Mint", "Nature", "City", "Plant", "Cement", "Advancement", "Destruction", "Non"};
 
 static const char *basic[] = {
-	/* pronouns, use only subject pronouns */ "I","You","He","She","It","We","You","They",
+	/* pronouns */ "I","You","He","She","It","We","You","They","Me","His","Hers","Its","Theirs","Ours","Mine","Yours",
 	/* question words */ "Who","What","When","Where","Why","Which","Whose","How","With","Without",
 	/* coordination */ "For","And","Nor","Not","But","Or","Yet","So","Else","To",
 	/* location */ "At","In","Under","Above","Over","Below","On",
 	/* determiners */ "A","An","That","This","These","Those","Is",
-		/* |-> quantifiers */ "All","None","Some","Few","Most",
+	/* quantifiers */ "All","None","Some","Few","Most",
 	/* time */ "Now","Never","Before","After","During","Since","Until",
 	/* direction */ "To","From","Into","Out","From","Trough","Besides",
 	/* frequency */ "Always","Often","Sometimes","Rarely","Never",
