47191d25625330ec7817e55d4319a50105f5e8a8
Undeleted <zephimhere@gmail.com>
2026-02-23 14:18:37 -0300

    utils.h maintenance

--- src/utils.h
+++ src/utils.h
@@ -11,16 +11,14 @@ int *mapflags(int argc, char *argv[], int arr[]) {
 	/* the flags the program will check */
 	const char *pflags[] = {"--help","-h","-i"};
 	const int tflags = sizeof(pflags) / sizeof(pflags[0]);
-	/* set all entries to -1 by default */
-	memset(arr,-1,sizeof(*arr));
 
 	for (int i = 0; i < argc; i++) {
 		arr[i] = -1; /* by default, entry is -1 as it didnt find a match */
 
-		for (int j = 0; j < tflags; j++) { /* j will be the entry of pflags incase there is a match */
-			if (strcmp(argv[i], pflags[j]) == 0) { /* if the entry matches one of the entries inside pflags*/
-				arr[i] = j; /* pflags gets translated into an int (j) and then put into map */
-				break;
+		for (int j = 0; j < tflags; j++) {
+			if (strcmp(argv[i], pflags[j]) == 0) {
+				arr[i] = j;
+				break; /* for i loop restarts for j loop */
 			}
 		}
 	}
