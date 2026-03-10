a11810acf26dbb024372172e50a5277ec5f17af5
usr-undeleted <zephimhere@gmail.com>
2026-03-10 16:34:03 -0300

    version 5.0.0

--- src/code.c
+++ src/code.c
@@ -1,10 +1,5 @@
 // -------NOTES AND OBJECTIVES-------
-// 1. all flag funcs have to take in *some* int due to limitations of an array of pointers to funcs
-// 2. current code only checks argv[1] for a flag
-// 3. put all funcs that need to proccess data in utils.h, words in words.h, and flag functions here
-// ***4. WORK ON -F FLAG:
-// implement picking wether output is infinite or not (check if -i is somewhere)***
-// have one of the args be the number of words outputted
+// 1. put all funcs that need to proccess data in utils.h, words in words.h, and flag functions here
 
 #include <stdio.h>
 #include <string.h>
@@ -23,26 +18,36 @@ typedef unsigned int uint;
 #define CLEAR() printf("\033c")
 
 void noflag(int num); /* flagless */
-void fhelp(int num); /* -h and --help */
+void fhelp(); /* -h and --help */
 void fsaywords(int num); /* -i flag */
-void fprintfile(int argc); /* -f flag  */
-void fcolor(int num); /* -c flag */
+void fprintfile(int num, int argc, char*argv[]); /* -f flag  */
+void fcolor(int num, int argc, char *argv[]); /* -c flag */
 
 uint lenwords, lenmarks, lenbasic; /* arr length for words, marks and basics */
-uint rwords, rmarks, rbasic, rcustom; /* random value for words, marks, basics and custom words */
+int cuslen; /* len of cus */
+uint rwords, rmarks, rbasic, rcustom, rclr; /* random value for words, marks, basics and custom words */
 int seed = 0; /* seed edited in for loop */
-int markc; /* used to type a mark at the end everytime */
 
-int usewords = 1; /* used to correctly print words */
-int shouldstop = 0; /* the loop should stop */
-int fileflag = 0; /* determines what words to print (from words.h or custom file) */
+/* replace with single uint8_t and use bitwise stuff to read */
+uint markc; /* used to type a mark at the end everytime */
+uint usewords = 1; /* used to correctly print words */
+uint shouldstop = 0; /* the loop should stop. by default, assumes -i */
+uint fileflag = 0; /* determines what words to print (from words.h or custom file) */
+uint infexists = 0; /* check if -i exists */
+uint clrexists = 0; /* check if -c exists */
+uint changenum = 1; /* if num should be changed in the func */
 
 char cus[10000][30]; /* file words */
-int cuslen; /* len of cus */
 FILE *fd; /* custom file */
 
-int globargc; /* global argc */
-int rclr; /* decides random colors */
+/* funcs match flags in utils.h */
+enum flags {
+	HELP,
+	H,
+	I,
+	F,
+	C
+};
 
 /* ansi colors for normal text colors, bg colors, and resetting the color */
 /* intentionally only edited later */
@@ -76,87 +81,67 @@ char *bgs[] = {
 char *reset = "\033[0m";
 
 int arr[10]; /* contains the vals for the flags */
+int firstflag; /* first flag, aka the one that does stuff */
+int indexflag = -1; /* index of arr[n] for firstflag */
 
 /* objective: check for validity when calling the command */
 int main (int argc, char* argv[]) {
-	int num = 0; /* arg 1*/
-	globargc = argc;
-	void (*flagfuncs[])(int) = {fhelp,fhelp,fsaywords,fprintfile,fcolor}; /* funcs match flags in utils.h */
-	mapflags(argc,argv,arr);
-
-	fd = fopen(argv[2], "r");
+	int num = atoi(argv[1]); /* num user inputs */
+	mapflags(argc,argv,arr); /* arr contains flags */
 
 	if (argc < 2) {
-		fputs("Not enough arguments! See -h or --help for help.\n", stderr);
+		fprintf(stderr, "Too little args! See -h or --help for help.\n");
 		return 1;
 	}
 
-	int infexists = 0; /* use this for loop to also check if -i exists */
-	int clrexists = 0; /* check if -c exists */
+	/* determine first flag */
+	for (int i = 0; i < argc; i++) {
+		if (arr[i] != -1) {
+			firstflag = arr[i];
+			indexflag = i;
+			break;
+		}
+	}
 
+	/* check if -i or -c exist */
 	for (int i = 0; i < argc; i++) {
 		if (arr[i] == 2) {
 			infexists = 1;
 		}
+
 		if (arr[i] == 4) {
 			clrexists = 1;
 		}
 	}
 
-	/* get the value of num by checking the next num after a flag like -c */
-	if (argc > 2) {
-		for (int i = 0; i < argc; i++) {
-			num = atoi(argv[1]);
+	/* incase no flag */
+	if (arr[1] == -1) {
+		noflag(num);
+		exit(1);
+	}
 
-
-			if (arr[i] != -1) {
-				num = atoi(argv[i + 1]);
-				break;
+	enum flags pick = firstflag;
+	switch (pick) {
+		case 0:
+			if (indexflag != -1) {
+				fhelp();
 			}
-		}
-	} else {
-		num = atoi(argv[1]);
+			break;
+		case 1:
+			fhelp();
+			break;
+		case 2:
+			fsaywords(num);
+			break;
+		case 3:
+			fprintfile(num, argc, argv);
+			break;
+		case 4:
+			fcolor(num, argc, argv);
+			break;
 	}
 
-	for (int i = 0; i < argc; i++) {
-		if (arr[i] == 0 || arr[i] == 1) {
-			fhelp(argc);
-			return 0;
-		}
-	}
 
-	/* i am so, so sorry */
-	if (num < 1 && infexists == 0 && clrexists == 0) {
-		fprintf(stderr, "Number is not valid! See -h or --help for help.\n");
-		return 6;
-	}
-
-	/* if the flag is -f, set num appropriately */
-	if (!strcmp(argv[1], "-f") && argc >= 4) {
-		fileflag = 1;
-		num = atoi(argv[3]);
-		if (strcmp(argv[3], "-i")) {
-			shouldstop = 1;
-		}
-	}
-
-	if (arr[1] != -1) { /* always place flag check b4 flagless check */
-		(*flagfuncs[arr[1]])(num);
-		return 0;
-	}
-
-	if (arr[1] == -1 && num < 1) { /* incase first check fails, send an error message */
-		fprintf(stderr, "Secondary argument isn't valid! See -h or --help for help.\n");
-		return 2;
-	}
-
-	/* stuff that prints */
-	if (argc == 2) {
-		noflag(num); /* start default func */
-	} else {
-		fputs("Too many arguments! See -h or --help for help.\n", stderr);
-		return 3;
-	}
 	return 0;
 };
 
@@ -171,7 +156,7 @@ void fsaywords(int num) {
 	lenbasic = sizeof(basic) / sizeof(basic[0]);
 	/* cuslen is edited b4 this */
 
-	if (shouldstop == 0) {
+	if (!shouldstop) {
 		num = -1;
 	}
 
@@ -224,7 +209,7 @@ void noflag(int num) {
 	fsaywords(num);
 };
 
-void fhelp(int argc) { /* has to take in *some* int */
+void fhelp() {
 	fputs("Thank you for using talktokernel! Usage:\n"
 	"	<command> <flag> <flaginput>\n"
 	"\n"
@@ -234,25 +219,26 @@ void fhelp(int argc) { /* has to take in *some* int */
 	"	-h or --help: Displays this help menu.\n"
 	"	-i (infinite): prints nonstop. Takes in no arguments. \n"
 	"	-f (read from file) takes words from file and replaces the printable words with the ones in the file.\n"
-	"	Usage: ./binary -f <file> <num>\n"
+	"	Usage: ./binary -f <file> <num> <-c>\n"
 	"		Num can be either -i for infinite, or any number you desire.\n"
+	"		If -c is specified, print with color.\n"
 	"	-c (color) EPILEPSY WARNING!!! Prints words/cells with color\n"
 	"	Usage: ./binary -c <num>\n"
 	"		Num can either be a valid num or -i, and if unset, colors cells for a trippy AND EPILEPSY-INDUCING EFFECT!!! BE AWARE!! I AM GONNA WARN YOU MULITPLE TIMES!! I AM NOT RESPONSIBLE FOR ANY EPILEPSY ATTACKS THE USER MIGHT HAVE!!\n", stderr);
 	exit(0);
 }
 
-void fprintfile(int num) {
+void fprintfile(int num, int argc, char*argv[]) {
 	/* get file, separate its words onto strings, and plop them onto an array (data) */
+	fd = fopen(argv[indexflag + 1], "r");
 
+	if (argc < 4) {
+		fputs("Too little args! See -h or --help for help.\n", stderr);
+		exit(2);
+	}
 	if (fd == NULL) {
 		fputs("Error! File couldn't be opened. Does the file exist or you have the proper permissions?\n", stderr);
-		exit(1);
-	}
-
-	if (globargc < 4) {
-		fputs("Too little args! See -h or --help for help.\n", stderr);
-		exit(4);
+		exit(10);
 	}
 
 	int i = 0;
@@ -261,12 +247,29 @@ void fprintfile(int num) {
 	}
 	cuslen = i;
 
-	fsaywords(num);
+	num = atoi(argv[indexflag + 2]);
 
+	/* incase input truly is invalid */
+	if (num < 1 && !infexists) {
+		fprintf(stderr, "Invalid number! See -h or --help for help.\n");
+		exit(3);
+	}
+
+	if (!infexists) {
+		shouldstop = 1;
+	}
+
+	fileflag = 1;
+	if (clrexists) {
+		changenum = 0;
+		fcolor(num,argc,argv);
+	}
+	fsaywords(num);
 	fclose(fd);
+	exit(0);
 }
 
-void fcolor (int num) {
+void fcolor (int num, int argc , char *argv[]) {
 	/* if succesful, set the ansi color values to their appropriate thing so that
 	when the main func prints, it wont just print nothing when it tries to print
 	the color codes. */
@@ -290,7 +293,7 @@ void fcolor (int num) {
 	bgs[7] = "\033[47m";
 	bgs[8] = "\033[40m";
 
-	if (globargc == 2) {
+	if (argc == 2) {
 		while (1) {
 			getrandom(&seed, sizeof(seed), 0);
 			srand(seed);
@@ -299,19 +302,22 @@ void fcolor (int num) {
 		}
 	}
 
-	if (globargc < 3) {
-		printf("Too little args! See -h or --help for help.\n");
-		exit(4);
+	/* if it this gets invocked by -f, for example */
+	if (changenum) {
+		num = atoi(argv[indexflag + 1]);
 	}
 
-	if (arr[2] == 2) { // if -i
+	if (argc < 3) {
+		fprintf(stderr, "Too little args! See -h or --help for help.\n");
+		exit(1);
+	}
+
+	if (infexists) {
 		shouldstop = 0;
-	} else if (arr[2] == -1) { // if just regular num
-		shouldstop = 1;
 	} else {
-		printf("Incorrect usage! See -h or --help for help.\n");
-		exit(5);
+		shouldstop = 1;
 	}
-
 	fsaywords(num);
+
+	exit(0);
 }
